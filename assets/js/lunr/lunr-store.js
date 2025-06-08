6402c4f0e977c38d9d03c9e021d12056339b4c63
insightkim-phd <insightkim-phd@users.noreply.github.com>
2025-06-08 15:42:11 +0000

    deploy: 4efac839788e08f6fdce263cbeab45d617128d38

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "About",
+        "excerpt":"DAILab은 데이터 분석, 인공지능, 스마트팩토리, HR 애널리틱스를 중심으로 연구하는 연구실입니다.  ","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Members",
+        "excerpt":"Members      Prof. Hyojoong Kim (PI)   Graduate Students   Undergraduate Interns  ","categories": [],
+        "tags": [],
+        "url": "/members/",
+        "teaser": null
+      },{
+        "title": "Research",
+        "excerpt":"Research Areas     Smart Factory Anomaly Detection   HR Skill-based Optimization   Large Language Model Applications  ","categories": [],
+        "tags": [],
+        "url": "/research/",
+        "teaser": null
+      },{
+        "title": "Welcome to DAILab",
+        "excerpt":"DAILab 홈페이지에 오신 것을 환영합니다!  ","categories": [],
+        "tags": [],
+        "url": "/2025/06/08/welcome.html",
+        "teaser": null
+      }]
